afd3e9bdf93761887141acdb65dfdf26c503afc0
AbdElRahman <123567756+AbdElRahmanAwd@users.noreply.github.com>
2025-07-03 01:57:00 +0300

    fix: dublicated React

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import { store } from "@/store/store";
 import App from "@/App";
 
-import React from "react";
 import { PopupProvider } from "@/contexts/PopupContext"; // ✅
 
 
