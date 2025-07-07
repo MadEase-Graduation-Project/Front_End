1b7c6ba64969f01ea86eb3360221726704c761fc
Tasneemfahmi <tasneemfahmimadkour@gmail.com>
2025-07-07 21:42:40 +0300

    fixed conflict error

--- src/pages/main/Community/Community.jsx
+++ src/pages/main/Community/Community.jsx
@@ -15,7 +15,7 @@ import {
   sortedAdvices,
   selectFilteredAdvices,
 } from "@/store/selectors/index";
-import Popup from "./components/Popup";
+import Popup from "@/components/shared/Popup";
 import { useLocation } from "react-router-dom";
 
 export default function Community() {
